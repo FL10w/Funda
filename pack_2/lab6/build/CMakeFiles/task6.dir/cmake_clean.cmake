6a46d8e4bc2e5ab527028c3461239d812b42f270
FL10w <shitovnv@gmail.com>
2025-11-08 16:02:33 +0300

    Add files via upload

---
+++ pack_2/lab6/build/CMakeFiles/task6.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task6.dir/link.d"
+  "CMakeFiles/task6.dir/main.c.o"
+  "CMakeFiles/task6.dir/main.c.o.d"
+  "CMakeFiles/task6.dir/student.c.o"
+  "CMakeFiles/task6.dir/student.c.o.d"
+  "task6"
+  "task6.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/task6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
